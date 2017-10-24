1bfdd3dd745191b017ebd32200951afefafb7b23
chibitofu <erinmoony@gmail.com>
2017-10-24 15:05:40 -0700

    Refactored Person class to conform to NSCoder.

--- name_to_faces/Person.swift
+++ name_to_faces/Person.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 //Custom object to hold invidiual pictures attributes.
-class Person: NSObject {
+class Person: NSObject, NSCoding {
+    required init(coder aDecoder:  NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as! String
+        image = aDecoder.decodeObject(forKey: "image") as! String
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+    
     var name: String
     var image: String
 
-    init(name: String, image: String) {
+    required init(name: String, image: String) {
         self.name = name
         self.image = image
     }
